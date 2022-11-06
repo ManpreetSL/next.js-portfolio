bc3d3ff40e66e079571537d9d647755a2b73ccc0
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-11-06 16:44:35 +0000

    Added basic Header layout

---
+++ components/Header.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-13 xl-items-center'>
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+      >
+        <SocialIcon url='https://twitter.com/ManpreetSL' />
+        <SocialIcon url='https://www.youtube.com/channel/UCu0nuFOUfymXD_fw4Nh70ag' />
+      </motion.div>
+      <motion.div
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1
+        }}
+      >
+        <SocialIcon
+          className='cursor-pointer'
+          network='email'
+          fgColor='gray'
+          bgColor='transparent'
+        />
+        <p className='hidden md:inline-flex text-sm text-gray-400'>
+          Get in touch :)
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
